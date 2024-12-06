f7a3ca5d9ae8865e5fc8c76bc9475e0c707cd624
RoneySoares <cig.roney.conta@gmail.com>
2024-12-06 20:16:54 -0300

    Add files via upload

---
+++ Backend/conta.java
@@ -0,0 +1,12 @@
+package Conta;
+public class conta {
+    protected double saldo;
+
+    public conta(double saldoInicial){
+        this.saldo = saldoInicial;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
