2cce35aa557f7a456c45ecf25c7a1ce7e2d10c7a
Roney <cig.roney.conta@gmail.com>
2024-12-07 15:29:19 -0300

    Framework

---
+++ Framework/spring_vscode/src/main/java/com/roney/spring_vscode/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.roney.spring_vscode.controller;
+
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
+
+@RegisterReflectionForBinding
+
+
+public class HelloController {
+    
+
+}
